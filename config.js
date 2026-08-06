cee3b9d4efa87cf48d7546cb35b5cbb186ecd12e
MASSAMAN-max <higawoodholly@gmail.com>
2026-08-06 15:17:21 +0900

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+/**
+ * =====================================
+ * 環境依存パラメータ（デプロイごとに異なる値をここへ設定する。）
+ * =====================================
+ */
+const CONFIG = {
+  GAS_URL: 'https://script.google.com/macros/s/AKfycbyEDEDqBM9EsSS0Vtm2foHbybAG-q0IqjJhE0dgwL621vFcZywT4DcVHTIkfUkjgzcP0w/exec'
+};
