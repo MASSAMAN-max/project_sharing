6ee971f7b82a7cb7d9364793acd6e6f829406d4e
MASSAMAN-max <higawoodholly@gmail.com>
2026-09-11 17:15:59 +0900

    Update config.js

--- config.js
+++ config.js
@@ -6,3 +6,9 @@
 const CONFIG = {
   GAS_URL: 'https://script.google.com/macros/s/AKfycbyEDEDqBM9EsSS0Vtm2foHbybAG-q0IqjJhE0dgwL621vFcZywT4DcVHTIkfUkjgzcP0w/exec'
 };
+  // LINEログインチャネルのLIFF ID。
+  // 案件共有アプリ（index.html）のLINE内蔵ブラウザ認証と、
+  // share.html（共有方法選択ページ）のLINE送信リンク生成の両方で使う値を
+  // ここに一元化した（以前はindex.html側にハードコードされていた）。
+  LIFF_ID: '2011463016-0MGVvN6M'
+};
